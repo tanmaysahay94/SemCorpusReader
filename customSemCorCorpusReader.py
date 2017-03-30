50e8f829703c7b1886d8397b5d44e2101d0bf517
Tanmay Sahay <tanmaysahay94@gmail.com>
2017-03-30 18:07:55 +0530

    Cleaned namespace

--- customSemCorCorpusReader.py
+++ customSemCorCorpusReader.py
@@ -6,33 +6,48 @@ from lxml import html
 
 class CustomSemCorCorpusReader(object):
 
-    def __init__(self, pathToSemCorCorpus, fileName='br-a01'):
-        self.pathToSemCorCorpus     = None
-        self.fileName               = None
-        self.pathToFile             = None
+    def __init__(self, pathToSemCorCorpus, fileName='br-a02'):
+        self._pathToSemCorCorpus    = None
+        self._fileName              = None
+        self._pathToFile            = None
         self._raw                   = None
         self._annotated_chunks      = None
         self._wnsn_tagged_chunks    = None
         self._pos_tagged_chunks     = None
         self._lemma_tagged_chunks   = None
+        self._sents                 = None
+        self._fileids               = None
         try:
             assert os.path.exists(pathToSemCorCorpus) is True
-            self.pathToSemCorCorpus = pathToSemCorCorpus
+            self._pathToSemCorCorpus = pathToSemCorCorpus
             try:
-                self.pathToFile         = self.findPathToFile(fileName, pathToSemCorCorpus)
+                def _findPathToFile(target, directory):
+                    for root, dirs, files in os.walk(directory):
+                        for name in files:
+                            if name == target:
+                                return os.path.join(root, name)
+                    return None
+                self._pathToFile = _findPathToFile(fileName, pathToSemCorCorpus)
                 assert self.pathToFile is not None
-                self.fileName           = fileName
+                self._fileName = fileName
             except:
                 raise IOError("{} not found in {}...".format(fileName, pathToSemCorCorpus))
         except:
             raise IOError("{} is an invalid filepath...".format(pathToSemCorCorpus))
 
-    def findPathToFile(self, target, directory):
-        for root, dirs, files in os.walk(directory):
-            for name in files:
-                if name == target:
-                    return os.path.join(root, name)
-        return None
+    def pathToSemCorCorpus(self):
+        return self._pathToSemCorCorpus
+
+    def fileName(self):
+        return self._fileName
+
+    def pathToFile(self):
+        return self._pathToFile
+
+    def fileids(self):
+        if self._fileids is None:
+            pass
+        return self._fileids
 
     def raw(self):
         if self._raw is None:
@@ -72,3 +87,9 @@ class CustomSemCorCorpusReader(object):
         if self._lemma_tagged_chunks is None:
             self._lemma_tagged_chunks = self._attribute_chunks('lemma')
         return self._lemma_tagged_chunks
+
+    def sents(self):
+        if self._sents is None:
+            self._sents = [' '.join(tup[1][0] for tup in annotated_sentence) \
+                            for annotated_sentence in self.annotated_chunks()]
+        return self._sents
